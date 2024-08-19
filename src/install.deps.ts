7965da4aa7df96ee05bc4ae26ab5e69bb7b9d225
mcgear <michael.gearhardt@gmail.com>
2024-08-19 14:33:19 -0600

    ref updates

--- src/install.deps.ts
+++ src/install.deps.ts
@@ -3,6 +3,5 @@ export * as colors from 'jsr:@std/fmt@1/colors';
 export * as path from 'jsr:@std/path@1';
 export { toText } from 'jsr:@std/streams@1/to-text';
 
-export * from 'jsr:@fathym/common@0/utils/path/exists';
-export * from 'jsr:@fathym/common@0/utils/merge';
-export * from 'jsr:@fathym/eac@0/utils/library';
+export * from 'jsr:@fathym/common@0/path';
+export * from 'jsr:@fathym/common@0/merge';
