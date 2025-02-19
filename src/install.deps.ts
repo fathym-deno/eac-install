e81d776e48ff51a3c387495e96f13b167b8c5036
mcgear <michael.gearhardt@gmail.com>
2025-02-18 17:46:31 -0700

    ref updates

--- src/install.deps.ts
+++ src/install.deps.ts
@@ -1,9 +1,9 @@
-export { parseArgs } from 'jsr:@std/cli@1';
-export * as colors from 'jsr:@std/fmt@1/colors';
-export { parse as parseJsonc } from 'jsr:@std/jsonc@1.0.0';
-export * as path from 'jsr:@std/path@1';
-export { toText } from 'jsr:@std/streams@1.0.6/to-text';
+export { parseArgs } from 'jsr:@std/cli@1.0.13';
+export * as colors from 'jsr:@std/fmt@1.0.5/colors';
+export { parse as parseJsonc } from 'jsr:@std/jsonc@1.0.1';
+export * as path from 'jsr:@std/path@1.0.1';
+export { toText } from 'jsr:@std/streams@1.0.9/to-text';
 
-export * from 'jsr:@fathym/common@0.2.173/path';
-export * from 'jsr:@fathym/common@0.2.173/merge';
-export type { DenoConfig } from 'jsr:@fathym/common@0.2.173/build';
+export * from 'jsr:@fathym/common@0.2.178/path';
+export * from 'jsr:@fathym/common@0.2.178/merge';
+export type { DenoConfig } from 'jsr:@fathym/common@0.2.178/build';
