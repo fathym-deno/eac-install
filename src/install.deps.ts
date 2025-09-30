c24ce3943d83768c0f3b23bdb74918f823b1eb02
mcgear <michael.gearhardt@gmail.com>
2025-09-29 22:41:15 -0600

    ref updates

--- src/install.deps.ts
+++ src/install.deps.ts
@@ -4,6 +4,6 @@ export { parse as parseJsonc } from 'jsr:@std/jsonc@1.0.2';
 export * as path from 'jsr:@std/path@1.1.2';
 export { toText } from 'jsr:@std/streams@1.0.12/to-text';
 
-export * from 'jsr:@fathym/common@0.2.267/path';
-export * from 'jsr:@fathym/common@0.2.267/merge';
-export type { DenoConfig } from 'jsr:@fathym/common@0.2.267/build';
+export * from 'jsr:@fathym/common@0.2.273/path';
+export * from 'jsr:@fathym/common@0.2.273/merge';
+export type { DenoConfig } from 'jsr:@fathym/common@0.2.273/build';
